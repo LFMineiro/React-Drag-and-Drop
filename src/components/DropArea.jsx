7047964d9fe4fce96d2d6f9486b1bb3549ce66af
LFMineiro <lflmalbuquerque3@gmail.com>
2024-10-04 09:49:27 -0300

    Initial commit

---
+++ src/components/DropArea.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import './DropArea.css'
+const DropArea = ({onDrop}) => {
+    const [showDrop, setShowDrop] = useState(false)
+    return (
+        <section 
+        onDragEnter={() => setShowDrop(true)}
+        onDragLeave={() => setShowDrop(false)}
+        className={showDrop ? 'drop_area' : 'hide_drop'}
+        onDrop={() => {
+            onDrop();
+            setShowDrop(false)
+        }}
+        onDragOver={(e) => e.preventDefault()}
+        >
+            Drop Area
+        </section>
+    )
+}
+
+export default DropArea
